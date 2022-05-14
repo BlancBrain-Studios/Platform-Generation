6473ec24c431471bd54dfc7229efc75c1f6e67fa
Holman57 <Holman57@gmail.com>
2022-05-14 13:03:49 -0700

    add falling boxes

--- Assets/Scripts/MakeBoxes.cs
+++ Assets/Scripts/MakeBoxes.cs
@@ -1,16 +1,22 @@
 using UnityEngine;
 using Random = UnityEngine.Random;
+using System.Collections;
 
 public class MakeBoxes : MonoBehaviour
 {
-    public GameObject prefab;
-    public int n = 300;
+	public GameObject alpha_block;
     void Start()
     {
-        for (int x = 0; x < n; x++)
-        {
-            var position = new Vector3(Random.Range(2, 15), Random.Range(0, 15), Random.Range(0, 10));
-            Instantiate(prefab, position, Quaternion.identity);
-        }
+         StartCoroutine(InfiniteLoop());
     }
+	public IEnumerator InfiniteLoop()
+	{
+    	WaitForSeconds waitTime = new WaitForSeconds(1);
+    	while (true)
+    	{
+        	var position = new Vector3(Random.Range(-5, 5), Random.Range(15, 50), Random.Range(-5, 5));
+        	GameObject gameObject = Instantiate(alpha_block, position, Quaternion.identity);
+			yield return waitTime;
+    	}
+	}
 }
